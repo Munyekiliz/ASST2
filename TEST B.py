4f528077c1e8b3a9406784e3c023f12f5705e9c8
Munyekiliz <81600858+Munyekiliz@users.noreply.github.com>
2021-04-15 15:15:44 -0400

    Add files via upload

---
+++ TEST B.py
@@ -0,0 +1,28 @@
+# Enter first and last names
+txt = "Name:""Liz Smith"
+x = txt.title()
+print(x)
+# Enter your country of origin
+txt = "Country:""Kenya"
+x = txt.title()
+print(x)
+# Enter the nature of your current job
+txt = "Profession:" "Childcare Manager"
+x = txt.title()
+print(x)
+# Enter your email address
+txt = "Email:" "lizk@gmail.com"
+x = txt.title()
+print(x)
+# Enter your phone number
+txt = "Phone:""123-654-7890"
+x = txt.title()
+print(x)
+# Enter your current city of residence
+txt = "City:""Tempe"
+x = txt.title()
+print(x)
+# Enter your mailing address
+txt = "Address""123 Grove Park"
+x = txt.title()
+print()
